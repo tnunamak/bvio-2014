f9629a62a3b458d800d74b0041e65f0c8d8bcd74
Tim Nunamaker <tim.nunamaker@bazaarvoice.com>
2014-04-03 15:16:15 -0500

    Load reviews from endpoint

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('bvio2014App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.reviews = ["this", "is", "test"]
+  .controller('MainCtrl', function($scope, $resource) {
+    var Reviews = $resource('http://10.247.2.132:9000/data/reviews.json?userId=rich&productId=:productId&friends=chris,stephen,tim');
+    Reviews.get({productId: 342649}).$promise.then(function(data) {
+      $scope.reviews = data.Results;
+    });
   });
