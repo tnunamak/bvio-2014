76ea7c9150ea95fe9224a8a98b231092a53bcc4f
chris pound <c2.pound@gmail.com>
2014-04-03 16:19:38 -0500

    added gplus login

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('bvio2014App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.reviews = ["this", "is", "test"]
+  .controller('MainCtrl', function($scope, $resource, $http) {
+    var Reviews = $resource('http://10.247.2.132:9000/data/reviews.json?userId=rich&productId=:productId&friends=chris,stephen,tim');
+    Reviews.get({productId: 342649}).$promise.then(function(data) {
+      $scope.reviews = data.Results;
+    });
+
+    $http.get('/someUrl').success(function(data) {
+      $scope.friends = data;
+    });
+
+    $scope.getNumber = function(num) {
+      return new Array(num);
+    };
   });
